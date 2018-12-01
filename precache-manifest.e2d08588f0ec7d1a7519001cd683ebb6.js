57222a1e0f39d147603a63a91c2dca5facc4a99a
JSMG95 <juansalim95@gmail.com>
2018-12-01 03:11:01 -0600

    Updates

---
+++ precache-manifest.e2d08588f0ec7d1a7519001cd683ebb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7486a8bdbc26ac914b6",
+    "url": "/anm-assignment/static/js/runtime~main.b7486a8b.js"
+  },
+  {
+    "revision": "ba85829a7fdfd2b094a5",
+    "url": "/anm-assignment/static/js/main.ba85829a.chunk.js"
+  },
+  {
+    "revision": "11df309bba4ac7d64eeb",
+    "url": "/anm-assignment/static/js/1.11df309b.chunk.js"
+  },
+  {
+    "revision": "ba85829a7fdfd2b094a5",
+    "url": "/anm-assignment/static/css/main.4ed2de3f.chunk.css"
+  },
+  {
+    "revision": "64c90a1dd91780912d91da5d3aa384b6",
+    "url": "/anm-assignment/index.html"
+  }
+];
